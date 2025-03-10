5d8bf5c434a8167f7cbf8eed659b110a1cefa644
czyt <czyt@w.cn>
2025-03-10 15:47:36 +0800

    调整代码

--- internal/handlers/userinfo.go
+++ internal/handlers/userinfo.go
@@ -4,26 +4,18 @@ import (
 	gohelper "gitee.com/linakesi/lzc-sdk/lang/go"
 	users "gitee.com/linakesi/lzc-sdk/lang/go/common"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
-type LoginInfo struct {
+type BasicInfo struct {
 	DeviceID      string
 	DeviceVersion string
 	UserId        string
 	UserRole      string
 }
 
-func listAllUers() []string {
-	var ret []string
-	dirs, err := os.ReadDir("/lzcapp/run/mnt/home")
-	if err != nil {
-		return nil
-	}
-	for _, d := range dirs {
-		ret = append(ret, d.Name())
-	}
-	return ret
+type LazyCatUser struct {
+	BasicInfo BasicInfo `json:"CurrentUserInfo"`
+	Detail    *users.UserInfo
 }
 
 func GetUserInfo(c *gin.Context) {
@@ -34,22 +26,21 @@ func GetUserInfo(c *gin.Context) {
 		return
 	}
 	defer gw.Close()
-	var ret struct {
-		CurrentUserInfo LoginInfo
-		Detail          *users.UserInfo
-	}
-	currentUser := LoginInfo{
+
+	currentUser := BasicInfo{
 		UserId:        c.GetHeader("x-hc-user-id"),
 		UserRole:      c.GetHeader("x-hc-user-role"),
 		DeviceID:      c.GetHeader("x-hc-device-id"),
 		DeviceVersion: c.GetHeader("x-hc-device-version"),
 	}
-
+	catUser := LazyCatUser{
+		BasicInfo: currentUser,
+	}
 	userInfo, err := gw.Users.QueryUserInfo(ctx, &users.UserID{Uid: currentUser.UserId})
 	if err != nil {
 		c.AbortWithError(500, err)
 		return
 	}
-	ret.Detail = userInfo
-	c.JSON(200, ret)
+	catUser.Detail = userInfo
+	c.JSON(200, catUser)
 }
